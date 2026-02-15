f2941a7f6ccd80e92db2c4b9670aff3d29dce12e
Gabo <gabotest301@gmail.com>
2026-02-15 19:09:07 -0300

    Se agrega pipeline para cybermap

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,7 @@ import 'dotenv/config';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
     env: {
       ...process.env,
     },
