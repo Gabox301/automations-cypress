dfc4f75b9e53be12bd2ee6c34fd7130ed2bf781a
Gabo <gabotest301@gmail.com>
2026-02-15 20:13:59 -0300

    Cambio en config

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'cypress';
 import 'dotenv/config';
 
 export default defineConfig({
-  allowCypressEnv: false,
   e2e: {
     setupNodeEvents(on, config) {},
     env: {
