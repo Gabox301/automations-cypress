559d194ab769e63b5327e079ab0714e2d41d4fdf
Gabo <gabotest301@gmail.com>
2026-02-16 20:48:03 -0300

    Refactor de comandos

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,37 +1,19 @@
 // For more comprehensive examples of custom commands please read more here: https://on.cypress.io/custom-commands
 
-// ***********************************************
-
 const timeout = 80000;
 
-Cypress.Commands.add('limpiar_redis', () => {
+Cypress.Commands.add('limpiar_base', (endpoint) => {
   cy.env(['API_TOKEN', 'API_URL']).then(({ API_TOKEN, API_URL }) => {
     cy.request({
       method: 'DELETE',
-      url: `${API_URL}/cache/redis`,
+      url: `${API_URL}/cache/${endpoint}`,
       body: {
         Token: API_TOKEN,
       },
       timeout: timeout,
     }).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.message).to.eq('Redis cache cleared');
-    });
-  });
-});
-
-Cypress.Commands.add('limpiar_turso', () => {
-  cy.env(['API_TOKEN', 'API_URL']).then(({ API_TOKEN, API_URL }) => {
-    cy.request({
-      method: 'DELETE',
-      url: `${API_URL}/cache/db`,
-      body: {
-        Token: API_TOKEN,
-      },
-      timeout: timeout,
-    }).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.message).to.eq('DB cache cleared');
+      expect(response.status, `Request realizado con éxito para ${endpoint}`).to.eq(200);
+      expect(response.body.message, `Limpieza de ${endpoint} exitosa`).to.include(`cache cleared`);
     });
   });
 });
