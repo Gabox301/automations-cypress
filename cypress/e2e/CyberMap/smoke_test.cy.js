70f7a60850e81993af2a11dfd543266dd5138053
Gabo <gabotest301@gmail.com>
2026-02-16 21:24:29 -0300

    Refactor pruebas

--- cypress/e2e/CyberMap/smoke_test.cy.js
+++ cypress/e2e/CyberMap/smoke_test.cy.js
@@ -1,28 +1,40 @@
 import datos from '../../fixtures/CyberMap.json';
 
-before(() => {
-  cy.limpiar_redis();
-  cy.limpiar_turso();
-});
+describe('Smoke Test CyberMap', () => {
+  before(() => {
+    cy.limpiar_base('redis');
+    cy.limpiar_base('db');
+  });
 
-describe('Gestión de datos en CyberMap', () => {
-  it('Debe limpiar datos existentes y validar sources de datos', () => {
+  beforeEach(() => {
     cy.visit(datos.base_url);
     cy.url().should('include', datos.base_url);
     cy.contains('Conectado', { timeout: 80000 }).should('be.visible');
     cy.get('.text-card-foreground', { timeout: 10000 }).should('be.visible');
     cy.wait(1000);
+  });
+
+  it('Validar source Nominatim', () => {
     cy.get('body').click('center');
     cy.contains('Nominatim').should('be.visible');
+    cy.get(datos.selectors.close_button).should('be.visible').click();
+  });
+
+  it('Validar source Upstash Redis', () => {
+    cy.get('body').click('center');
+    cy.contains('Redis').should('be.visible');
     cy.get(datos.selectors.close_button).should('be.visible').click();
     cy.get(datos.selectors.popup_details_button).should('be.visible').click();
     cy.contains('Redis').should('be.visible');
     cy.get(datos.selectors.close_button).should('be.visible').click();
-    cy.limpiar_redis();
+  });
+
+  it('Validar source Turso DB', () => {
+    cy.limpiar_base('redis');
     cy.reload();
     cy.contains('Conectado', { timeout: 80000 }).should('be.visible');
     cy.get('body').click('center');
-    cy.contains('Turso').should('be.visible');
+    cy.contains('Turso DB').should('be.visible');
     cy.get(datos.selectors.close_button).should('be.visible').click();
     cy.get(datos.selectors.popup_delete_button).should('be.visible').click();
   });
